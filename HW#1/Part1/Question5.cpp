7f8f1858cb183bcd04d5e85591630b8e6e7cfeb1
KevinInzu <45952314+KevinInzu@users.noreply.github.com>
2021-10-17 22:05:06 -0700

    Create Question5.cpp

---
+++ HW#1/Part1/Question5.cpp
@@ -0,0 +1,29 @@
+//test_signal.cpp
+#include <signal.h>
+#include <unistd.h>
+#include <iostream>
+ 
+using namespace std;
+ 
+void func (int sig)
+{
+  cout << "Oops! -- I got a signal " << sig << endl;
+}
+ 
+int main()
+{
+  struct sigaction sigbreak;
+ 
+  (void) signal (SIGINT, func);    //catch terminal interrupts
+  sigemptyset(&sigbreak.sa_mask);  // Sigbreak is class, sa_mask is variable which is the set of signals that we want delete
+  sigaction (SIGINT,&sigbreak,NULL);
+  
+  
+
+  
+  while (1){
+    cout << "CSUSB CSE 4600 Operating Systems. Homework on signals" << endl;
+    sleep (1); 
+  }
+  return 0;
+}
